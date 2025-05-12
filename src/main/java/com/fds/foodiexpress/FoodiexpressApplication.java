9c340472279cff9aa5ccdb5cabc03bf5a8b10f79
lalit-shelkar <lalitshelkar2424@gmail.com>
2025-05-12 12:30:31 +0530

    Projects Intialized

---
+++ src/main/java/com/fds/foodiexpress/FoodiexpressApplication.java
@@ -0,0 +1,13 @@
+package com.fds.foodiexpress;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodiexpressApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FoodiexpressApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fds/foodiexpress/FoodiexpressApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fds.foodiexpress;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FoodiexpressApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
